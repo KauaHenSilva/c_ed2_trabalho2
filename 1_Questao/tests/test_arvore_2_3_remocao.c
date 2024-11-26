f8a4c9d086651c632399a77794583d5283117588
KauaHenSilva <kaua.sbc@gmail.com>
2024-11-26 14:06:57 -0300

    feat: test update

--- 1_Questao/tests/test_arvore_2_3_remocao.c
+++ 1_Questao/tests/test_arvore_2_3_remocao.c
@@ -1,218 +1,125 @@
 #include "Unity/unity.h"
 #include "include/arvore_2_3.h"
 
-// ArvoreVermelhoPreto *raiz;
+Arvore_2_3 *raiz;
 
-// void prencher_automatico(int *valores, int qtd)
-// {
-//   for (int i = 0; i < qtd; i++)
-//   {
-//     ArvoreVermelhoPreto *newTree = NULL;
-//     aloca_arvore_vermelho_preto(&newTree);
-//     char nome_unidade[20];
-//     char palavra_portugues[20];
-//     sprintf(nome_unidade, "Unidade %d", valores[i]);
-//     sprintf(palavra_portugues, "%d", valores[i]);
-//     def_arvore_vermelho_preto(newTree, palavra_portugues, palavra_portugues, nome_unidade);
-//     inserir_arvore_vermelho_preto(&raiz, newTree);
-//   }
-// }
+void func_test_inserindo_valores_automatico(int *valores, int qtd)
+{
+  for (int i = 0; i < qtd; i++)
+  {
+    InfoMain newInfo;
+    char nome_unidade[20];
+    char palavra_portugues[20];
+    char unidade[20];
+
+    sprintf(nome_unidade, "%d", valores[i]);
+    sprintf(palavra_portugues, "%d", valores[i]);
+    sprintf(unidade, "%d", valores[i]);
+
+    InfoMain promove;
+    Arvore_2_3 *pai = NULL;
+    Arvore_2_3 *new_node = NULL;
+
+    def_info_arvore_2_3(&newInfo, palavra_portugues, palavra_portugues, nome_unidade);
+    inserir_arvore_2_3(&raiz, newInfo, &promove, &pai, &new_node);
+  }
+}
 
 void setUp(void)
 {
-  // raiz = NULL;
+  raiz = NULL;
 }
 
 void tearDown(void)
 {
-  // free_arvore_vermelho_preto(&raiz);
+  free_arvore_2_3(raiz);
 }
 
-// void test_remocao_arvore_vermelha_preta_caso_1(); // inc: 4, 2, 5, 1, 3 -> rem: 3
-// void test_remocao_arvore_vermelho_preto_caso_2(); // inc: 4, 2, 5, 1, 3 -> rem: 2
-// void test_remocao_arvore_vermelho_preto_caso_3(); // inc: 4, 2, 5, 1, 3 -> rem: 4
-// void test_remocao_arvore_vermelho_preto_caso_4(); // inc: 4, 2, 5, 1, 3 -> rem: 5
-// void test_remocao_arvore_vermelho_preto_caso_5(); // inc: 4, 2, 5, 1, 3 -> rem: 1
-// void test_remocao_arvore_vermelho_preto_caso_6(); // inc: 3 -> rem: 2
-// void test_remocao_arvore_vermelho_preto_caso_7(); // inc: 3 -> rem: 3
-// void test_remocao_arvore_vermelho_preto_caso_8(); // inc: 3 -> rem: 4
+void test_remocao_2_3_caso_1(); // inc: 1 -> rem: 1
+void test_remocao_2_3_caso_2(); // inc: 1, 2 -> rem: 1
+void test_remocao_2_3_caso_3(); // inc: 1, 2 -> rem: 2
+void test_remocao_2_3_caso_4(); // inc: 1, 2, 3 -> rem: 1
+void test_remocao_2_3_caso_5(); // inc: 1, 3, 2, 5, 4 -> rem: 1
 
 int main()
 {
   UNITY_BEGIN();
-  // RUN_TEST(test_remocao_arvore_vermelha_preta_caso_1);
-  // RUN_TEST(test_remocao_arvore_vermelho_preto_caso_2);
-  // RUN_TEST(test_remocao_arvore_vermelho_preto_caso_3);
-  // RUN_TEST(test_remocao_arvore_vermelho_preto_caso_4);
-  // RUN_TEST(test_remocao_arvore_vermelho_preto_caso_5);
-  // RUN_TEST(test_remocao_arvore_vermelho_preto_caso_6);
-  // RUN_TEST(test_remocao_arvore_vermelho_preto_caso_7);
-  // RUN_TEST(test_remocao_arvore_vermelho_preto_caso_8);
+  RUN_TEST(test_remocao_2_3_caso_1);
+  RUN_TEST(test_remocao_2_3_caso_2);
+  RUN_TEST(test_remocao_2_3_caso_3);
+  RUN_TEST(test_remocao_2_3_caso_4);
+  RUN_TEST(test_remocao_2_3_caso_5);
   return UNITY_END();
 }
 
-// void test_remocao_arvore_vermelha_preta_caso_1()
-// {
-//   int valores_adicinar[] = {4, 2, 5, 1, 3};
-//   prencher_automatico(valores_adicinar, 5);
+void test_remocao_2_3_caso_1()
+{
+  int valores_adicinar[] = {1};
+  func_test_inserindo_valores_automatico(valores_adicinar, 1);
 
-//   int confirmacao = remover_arvore_vermelho_preto(&raiz, "3");
+  Arvore_2_3 *maior;
+  int resposta = remover_arvore_2_3(&raiz, "1", NULL, &raiz, &maior);
 
-//   TEST_ASSERT_EQUAL_INT(1, confirmacao);
-//   TEST_ASSERT_EQUAL_STRING("4", raiz->info.palavra_portugues);
-//   TEST_ASSERT_EQUAL_STRING("2", raiz->esq->info.palavra_portugues);
-//   TEST_ASSERT_EQUAL_STRING("5", raiz->dir->info.palavra_portugues);
-//   TEST_ASSERT_EQUAL_STRING("1", raiz->esq->esq->info.palavra_portugues);
+  TEST_ASSERT_EQUAL_INT(1, resposta);
+  TEST_ASSERT_NULL(raiz);
+}
 
-//   TEST_ASSERT_EQUAL_INT(PRETO, raiz->cor);
-//   TEST_ASSERT_EQUAL_INT(PRETO, raiz->esq->cor);
-//   TEST_ASSERT_EQUAL_INT(PRETO, raiz->dir->cor);
-//   TEST_ASSERT_EQUAL_INT(VERMELHO, raiz->esq->esq->cor);
+void test_remocao_2_3_caso_2()
+{
+  int valores_adicinar[] = {1, 2};
+  func_test_inserindo_valores_automatico(valores_adicinar, 2);
 
-//   TEST_ASSERT_NULL(raiz->dir->esq);
-//   TEST_ASSERT_NULL(raiz->dir->dir);
-//   TEST_ASSERT_NULL(raiz->esq->dir);
-//   TEST_ASSERT_NULL(raiz->esq->esq->esq);
-//   TEST_ASSERT_NULL(raiz->esq->esq->dir);
-// }
+  Arvore_2_3 *maior;
+  int resposta = remover_arvore_2_3(&raiz, "1", NULL, &raiz, &maior);
 
-// void test_remocao_arvore_vermelho_preto_caso_2()
-// {
-//   int valores_adicinar[] = {4, 2, 5, 1, 3};
-//   prencher_automatico(valores_adicinar, 5);
+  TEST_ASSERT_EQUAL_INT(1, resposta);
+  TEST_ASSERT_EQUAL_INT(1, raiz->n_info);
+  TEST_ASSERT_EQUAL_STRING("2", raiz->info1.arv_binaria_palavra_ingles->info.palavra_ingles);
+  TEST_ASSERT_EQUAL_STRING("2", raiz->info1.arv_binaria_palavra_ingles->info.unidades->nome_unidade);
+  TEST_ASSERT_EQUAL_STRING("2", raiz->info1.palavra_portugues);
+}
 
-//   int confirmacao = remover_arvore_vermelho_preto(&raiz, "2");
+void test_remocao_2_3_caso_3()
+{
+  int valores_adicinar[] = {1, 2};
+  func_test_inserindo_valores_automatico(valores_adicinar, 2);
 
-//   TEST_ASSERT_EQUAL_INT(1, confirmacao);
-//   TEST_ASSERT_EQUAL_STRING("4", raiz->info.palavra_portugues);
-//   TEST_ASSERT_EQUAL_STRING("3", raiz->esq->info.palavra_portugues);
-//   TEST_ASSERT_EQUAL_STRING("5", raiz->dir->info.palavra_portugues);
-//   TEST_ASSERT_EQUAL_STRING("1", raiz->esq->esq->info.palavra_portugues);
+  Arvore_2_3 *maior;
+  int resposta = remover_arvore_2_3(&raiz, "2", NULL, &raiz, &maior);
 
-//   TEST_ASSERT_EQUAL_INT(PRETO, raiz->cor);
-//   TEST_ASSERT_EQUAL_INT(PRETO, raiz->esq->cor);
-//   TEST_ASSERT_EQUAL_INT(PRETO, raiz->dir->cor);
-//   TEST_ASSERT_EQUAL_INT(VERMELHO, raiz->esq->esq->cor);
+  TEST_ASSERT_EQUAL_INT(1, resposta);
+  TEST_ASSERT_EQUAL_INT(1, raiz->n_info);
+  TEST_ASSERT_EQUAL_STRING("1", raiz->info1.arv_binaria_palavra_ingles->info.palavra_ingles);
+  TEST_ASSERT_EQUAL_STRING("1", raiz->info1.arv_binaria_palavra_ingles->info.unidades->nome_unidade);
+  TEST_ASSERT_EQUAL_STRING("1", raiz->info1.palavra_portugues);
+}
 
-//   TEST_ASSERT_NULL(raiz->dir->esq);
-//   TEST_ASSERT_NULL(raiz->dir->dir);
-//   TEST_ASSERT_NULL(raiz->esq->dir);
-//   TEST_ASSERT_NULL(raiz->esq->esq->esq);
-//   TEST_ASSERT_NULL(raiz->esq->esq->dir);
-// }
+void test_remocao_2_3_caso_4()
+{
+  int valores_adicinar[] = {1, 2, 3};
+  func_test_inserindo_valores_automatico(valores_adicinar, 3);
+  int resposta = main_remover_arvore_2_3(&raiz, "1");
 
-// void test_remocao_arvore_vermelho_preto_caso_3()
-// {
-//   int valores_adicinar[] = {4, 2, 5, 1, 3};
-//   prencher_automatico(valores_adicinar, 5);
+  TEST_ASSERT_EQUAL_INT(1, resposta);
+  TEST_ASSERT_EQUAL_INT(2, raiz->n_info);
 
-//   int confirmacao = remover_arvore_vermelho_preto(&raiz, "4");
+  TEST_ASSERT_EQUAL_STRING("2", raiz->info1.arv_binaria_palavra_ingles->info.palavra_ingles);
+  TEST_ASSERT_EQUAL_STRING("3", raiz->info2.arv_binaria_palavra_ingles->info.palavra_ingles);
+}
 
-//   TEST_ASSERT_EQUAL_INT(1, confirmacao);
-//   TEST_ASSERT_EQUAL_STRING("2", raiz->info.palavra_portugues);
-//   TEST_ASSERT_EQUAL_STRING("1", raiz->esq->info.palavra_portugues);
-//   TEST_ASSERT_EQUAL_STRING("5", raiz->dir->info.palavra_portugues);
-//   TEST_ASSERT_EQUAL_STRING("3", raiz->dir->esq->info.palavra_portugues);
+void test_remocao_2_3_caso_5()
+{
+  int valores_adicionar[] = {1, 3, 2, 5, 4};
+  func_test_inserindo_valores_automatico(valores_adicionar, 5);
+  int resposta = main_remover_arvore_2_3(&raiz, "3");
 
-//   TEST_ASSERT_EQUAL_INT(PRETO, raiz->cor);
-//   TEST_ASSERT_EQUAL_INT(PRETO, raiz->esq->cor);
-//   TEST_ASSERT_EQUAL_INT(PRETO, raiz->dir->cor);
-//   TEST_ASSERT_EQUAL_INT(VERMELHO, raiz->dir->esq->cor);
+  TEST_ASSERT_EQUAL_STRING("2", raiz->info1.palavra_portugues);
+  TEST_ASSERT_EQUAL_INT(1, raiz->n_info);
 
-//   TEST_ASSERT_NULL(raiz->dir->dir);
-//   TEST_ASSERT_NULL(raiz->dir->esq->dir);
-//   TEST_ASSERT_NULL(raiz->dir->esq->esq);
-//   TEST_ASSERT_NULL(raiz->esq->dir);
-//   TEST_ASSERT_NULL(raiz->esq->esq);
-// }
+  TEST_ASSERT_EQUAL_STRING("1", raiz->esquerda->info1.palavra_portugues);
+  TEST_ASSERT_EQUAL_INT(1, raiz->n_info);
 
-// void test_remocao_arvore_vermelho_preto_caso_4()
-// {
-//   int valores_adicinar[] = {4, 2, 5, 1, 3};
-//   prencher_automatico(valores_adicinar, 5);
-
-//   int confirmacao = remover_arvore_vermelho_preto(&raiz, "5");
-
-//   TEST_ASSERT_EQUAL_INT(1, confirmacao);
-//   TEST_ASSERT_EQUAL_STRING("2", raiz->info.palavra_portugues);
-//   TEST_ASSERT_EQUAL_STRING("1", raiz->esq->info.palavra_portugues);
-//   TEST_ASSERT_EQUAL_STRING("4", raiz->dir->info.palavra_portugues);
-//   TEST_ASSERT_EQUAL_STRING("3", raiz->dir->esq->info.palavra_portugues);
-
-//   TEST_ASSERT_EQUAL_INT(PRETO, raiz->cor);
-//   TEST_ASSERT_EQUAL_INT(PRETO, raiz->esq->cor);
-//   TEST_ASSERT_EQUAL_INT(PRETO, raiz->dir->cor);
-//   TEST_ASSERT_EQUAL_INT(VERMELHO, raiz->dir->esq->cor);
-
-//   TEST_ASSERT_NULL(raiz->dir->dir);
-//   TEST_ASSERT_NULL(raiz->dir->esq->dir);
-//   TEST_ASSERT_NULL(raiz->dir->esq->esq);
-//   TEST_ASSERT_NULL(raiz->esq->dir);
-//   TEST_ASSERT_NULL(raiz->esq->esq);
-// }
-
-// void test_remocao_arvore_vermelho_preto_caso_5()
-// {
-//   int valores_adicinar[] = {4, 2, 5, 1, 3};
-//   prencher_automatico(valores_adicinar, 5);
-
-//   int confirmacao = remover_arvore_vermelho_preto(&raiz, "1");
-
-//   TEST_ASSERT_EQUAL_INT(1, confirmacao);
-//   TEST_ASSERT_EQUAL_STRING("4", raiz->info.palavra_portugues);
-//   TEST_ASSERT_EQUAL_STRING("3", raiz->esq->info.palavra_portugues);
-//   TEST_ASSERT_EQUAL_STRING("5", raiz->dir->info.palavra_portugues);
-//   TEST_ASSERT_EQUAL_STRING("2", raiz->esq->esq->info.palavra_portugues);
-
-//   TEST_ASSERT_EQUAL_INT(PRETO, raiz->cor);
-//   TEST_ASSERT_EQUAL_INT(PRETO, raiz->esq->cor);
-//   TEST_ASSERT_EQUAL_INT(PRETO, raiz->dir->cor);
-//   TEST_ASSERT_EQUAL_INT(VERMELHO, raiz->esq->esq->cor);
-
-//   TEST_ASSERT_NULL(raiz->dir->esq);
-//   TEST_ASSERT_NULL(raiz->dir->dir);
-//   TEST_ASSERT_NULL(raiz->esq->dir);
-//   TEST_ASSERT_NULL(raiz->esq->esq->esq);
-//   TEST_ASSERT_NULL(raiz->esq->esq->dir);
-
-// }
-
-// void test_remocao_arvore_vermelho_preto_caso_6()
-// {
-//   int valores_adicinar[] = {3};
-//   prencher_automatico(valores_adicinar, 1);
-
-//   int confirmacao = remover_arvore_vermelho_preto(&raiz, "2");
-
-//   TEST_ASSERT_EQUAL_INT(0, confirmacao);
-//   TEST_ASSERT_EQUAL_STRING("3", raiz->info.palavra_portugues);
-//   TEST_ASSERT_NULL(raiz->esq);
-//   TEST_ASSERT_NULL(raiz->dir);
-
-//   TEST_ASSERT_EQUAL_INT(PRETO, raiz->cor);
-// }
-
-// void test_remocao_arvore_vermelho_preto_caso_7()
-// {
-//   int valores_adicinar[] = {3};
-//   prencher_automatico(valores_adicinar, 1);
-
-//   int confirmacao = remover_arvore_vermelho_preto(&raiz, "3");
-
-//   TEST_ASSERT_EQUAL_INT(1, confirmacao);
-//   TEST_ASSERT_NULL(raiz);
-// }
-
-// void test_remocao_arvore_vermelho_preto_caso_8()
-// {
-//   int valores_adicinar[] = {3};
-//   prencher_automatico(valores_adicinar, 1);
-
-//   int confirmacao = remover_arvore_vermelho_preto(&raiz, "4");
-
-//   TEST_ASSERT_EQUAL_INT(0, confirmacao);
-//   TEST_ASSERT_EQUAL_STRING("3", raiz->info.palavra_portugues);
-//   TEST_ASSERT_NULL(raiz->esq);
-//   TEST_ASSERT_NULL(raiz->dir);
-//   TEST_ASSERT_EQUAL_INT(PRETO, raiz->cor);
-// }
+  TEST_ASSERT_EQUAL_STRING("4", raiz->direita->info1.palavra_portugues);
+  TEST_ASSERT_EQUAL_STRING("5", raiz->direita->info2.palavra_portugues);
+  TEST_ASSERT_EQUAL_INT(2, raiz->n_info);
+}
